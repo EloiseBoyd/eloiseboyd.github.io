7503c4eb32d94de53980fec3f07cb23709d14a62
EloiseBoyd <75344083+EloiseBoyd@users.noreply.github.com>
2020-12-03 23:24:19 +0000

    2nd Commit
    Images uploaded to folder & added to HTML for index and about pages
    
    Contact page & php code updated so that submit button should now work on form
    
    Javascript link code corrected in all html files

--- process.php
+++ process.php
@@ -4,12 +4,11 @@
 
 	if(isset($_POST['submit'])) {
 		
-	$to = "edwin@codingfaculty.com";
+	$to = "eloiseboyd@yahoo.co.uk";
 	$subject = $_POST['subject'];
 	$email = $_POST['email'];
 	$txt = $_POST['message'];
 	$headers = "From: " .$email . "\r\n" .
-	"CC: somebodyelse@example.com";
 	
 	mail($to,$subject,$txt,$headers);
 	
